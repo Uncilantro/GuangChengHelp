31bd41b3a091c76fa289d062195b5d30c71ab0e6
Dashboard <2072794161@qq.com>
2024-09-25 12:13:01 +0800

    Cheng

---
+++ GCP-Life-Help/pages/index/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+        <swiper-item>
+            <view class="swiper-item">itrm1</view>
+        </swiper-item>
+        <swiper-item>
+            <view class="swiper-item">item2</view>
+        </swiper-item>
+        <swiper-item>
+            <view class="swiper-item">item3</view>
+        </swiper-item>
+    </swiper>
+	
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+    .swiper-item{
+        background-color: green;
+        width: 800rpx;
+        height: 400rpx;
+        
+    }
+
+</style>
---
+++ GCP-Life-Help/pages/shop/shop.vue
@@ -0,0 +1,38 @@
+<template>
+    <view class="sell">
+    </view>
+    <view class="buy">
+        
+    </view>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+            
+        }
+    }
+</script>
+
+<style>
+    .sell{
+        flex: 1;
+        display: inline-block;
+        background-color: aqua;
+        height: 400rpx;
+        width: 400rpx;
+    }    
+    .buy{
+        flex: 1;
+        display: inline-block;
+        background-color: yellow;
+        height: 400rpx;
+        width: 400rpx;
+    }
+
+</style>
